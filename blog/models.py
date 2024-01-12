c21121ec094ee27071bd1f4a2f2a3522657d675e
Georgios Kopanidis <kopanidis@me.com>
2024-01-12 13:14:08 +0000

    Made migration

--- blog/models.py
+++ blog/models.py
@@ -13,3 +13,29 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+class Event(models.Model):
+    event_name = models.CharField(max_length=200, unique=True)
+    location = models.CharField(max_length=200)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return self.event_name
+
+class Ticket(models.Model):
+    ticket_holder = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="users_tickets"
+    )
+    date_issued = models.DateTimeField(auto_now_add=True)
+    event = models.ForeignKey(
+        Event,
+        on_delete=models.CASCADE,
+        related_name="event_tickets"
+    )
+
+    def __str__(self):
+        return f"Ticket for {self.ticket_holder}"
