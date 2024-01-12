9ecfdeb800aba3938e68a73d25c64d9cf6ef1242
Georgios Kopanidis <kopanidis@me.com>
2024-01-12 13:56:12 +0000

    Add comment class to models.py

--- blog/models.py
+++ blog/models.py
@@ -39,3 +39,12 @@ class Ticket(models.Model):
 
     def __str__(self):
         return f"Ticket for {self.ticket_holder}"
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
