cd2ecd015c9fc89dfc3196fa1c2f156482555eb0
Georgios Kopanidis <kopanidis@me.com>
2024-01-13 13:29:30 +0000

    Add generic post view

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["8000-gkopanidis-cipp4wtdjang-u2rh5opvb0f.ws-eu107.gitpod.io",'.herokuapp.com']
 
