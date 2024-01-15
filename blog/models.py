d9490752254e7bdb433943007991e9db6af4fecb
Georgios Kopanidis <kopanidis@me.com>
2024-01-15 11:37:29 +0000

    Add the about page

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -17,47 +19,21 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ["created_on"]
+        ordering = ["-created_on"]
 
     def __str__(self):
         return f"{self.title} | written by {self.author}"
 
 
-class Event(models.Model):
-    event_name = models.CharField(max_length=200, unique=True)
-    location = models.CharField(max_length=200)
-    date = models.DateTimeField()
-
-    def __str__(self):
-        return self.event_name
-
-
-class Ticket(models.Model):
-    ticket_holder = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name="users_tickets"
-    )
-    date_issued = models.DateTimeField(auto_now_add=True)
-    event = models.ForeignKey(
-        Event,
-        on_delete=models.CASCADE,
-        related_name="event_tickets"
-    )
-
-    def __str__(self):
-        return f"Ticket for {self.ticket_holder}"
-
-
 class Comment(models.Model):
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
-    approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
-    
+    approved = models.BooleanField(default=False)
+
     class Meta:
         ordering = ["created_on"]
 
--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,7 @@ class PostList(generic.ListView):
     template_name = "blog/index.html"
     paginate_by = 6
 
+
 def post_detail(request, slug):
     """
     Display an individual :model:`blog.Post`.
@@ -23,12 +24,6 @@ def post_detail(request, slug):
 
     :template:`blog/post_detail.html`
     """
-
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-
-    return render(
-        request,
-        "blog/post_detail.html",
-        {"post": post},
-    )
+    return render(request, "blog/post_detail.html", {"post": post},)
